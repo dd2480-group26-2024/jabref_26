6a236b11b893e3b85e102b9beb1c66aa20311ec9
MarcusJak <Marcusjakobsson00@gmail.com>
2024-02-21 20:51:12 +0100

    test #9 : added test cover branch row 135,137, 141
    added test to cover branch on rows 135,137, 141

--- src/test/java/org/jabref/logic/layout/LayoutHelperTest.java
+++ src/test/java/org/jabref/logic/layout/LayoutHelperTest.java
@@ -38,4 +38,13 @@ class LayoutHelperTest {
         Layout layout = layoutHelper.getLayoutFromText();
         assertNotNull(layout);
     }
+    @Test
+    public void testBracketedOptionFieldParsing() throws Exception {
+        StringReader stringReader = new StringReader("\\format[doi] DOI: \\doi");
+        LayoutHelper layoutHelper = new LayoutHelper(stringReader, layoutFormatterPreferences, abbreviationRepository);
+
+        Layout layout = layoutHelper.getLayoutFromText();
+
+        assertNotNull(layout);
+    }
 }
